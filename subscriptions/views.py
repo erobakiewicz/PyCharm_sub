312bfc830133a30d503032258c545297c4fb6531
erobakiewicz <erobakiewicz@gmail.com>
2021-01-11 17:50:48 +0100

    auth app with token auth

--- subscriptions/views.py
+++ subscriptions/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
 from subscriptions.models import Subscription
 from subscriptions.serializers import SubscriptionSerializer
 
 
 class SubscriptionViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsAuthenticated,)
     serializer_class = SubscriptionSerializer
     queryset = Subscription.objects.all()
