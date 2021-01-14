72cf474f971f7a62f6a3bdc0cd3d5fbb23bf8284
erobakiewicz <erobakiewicz@gmail.com>
2021-01-14 16:38:03 +0100

    test for adding monthly subscriptions

--- subscriptions/tests.py
+++ subscriptions/tests.py
@@ -5,7 +5,8 @@ from django.utils import timezone
 from rest_framework.test import APIClient
 
 from PyCharm_sub.factories import UserFactory, SubscriptionFactory
-from subscriptions.utils import check_if_user_has_valid_subscription
+from subscriptions.utils import check_if_user_has_valid_subscription, check_if_user_has_valid_type, \
+     check_if_valid_monthly_subscription_is_added
 
 client = Client()
 
@@ -30,18 +31,49 @@ class SubscriptionModelTestCase(TestCase):
         assert sub.client.id == 1
         assert sub.client.first_name == 'fname0'
 
+    def test_user_has_no_subscription(self):
+        has_sub = check_if_user_has_valid_subscription(self.user)
+        print((self.user.subscription_set).exists())
+        self.assertEqual(has_sub, True)
+
+
     def test_active_subscription(self):
-        SubscriptionFactory(is_active=True, client=self.user, sub_period=timezone.now() + timedelta(weeks=1))
+        sub0 = SubscriptionFactory(is_active=True, client=self.user, sub_period=timezone.now() + timedelta(weeks=1))
         has_valid_sub = check_if_user_has_valid_subscription(self.user)
+        print(sub0.sub_period)
         self.assertEqual(has_valid_sub, True)
 
     def test_inactive_subscription(self):
-        SubscriptionFactory(is_active=False, client=self.user)
+        sub =SubscriptionFactory(is_active=False, client=self.user)
         has_valid_sub = check_if_user_has_valid_subscription(self.user)
+        print(sub.sub_period)
         self.assertEqual(has_valid_sub, False)
 
-    def test_inactive_subscription_None(self):
+    def test_inactive_while_active(self):
         SubscriptionFactory(client=self.user, is_active=True)
         SubscriptionFactory(client=self.user, is_active=False)
         has_valid_sub = check_if_user_has_valid_subscription(self.user)
         self.assertEqual(has_valid_sub, True)
+
+    def test_active_subscription_monthly(self):
+        sub = SubscriptionFactory(is_active=True, client=self.user, sub_period=timezone.now() + timedelta(weeks=4),
+                            billing_type='monthly')
+        has_monthly_sub = check_if_valid_monthly_subscription_is_added(self.user)
+        print(sub.sub_period)
+        self.assertEqual(has_monthly_sub, True)
+
+    def test_user_type(self):
+        SubscriptionFactory(user_type="individual", client=self.user)
+        has_valid_type = check_if_user_has_valid_type(self.user)
+        self.assertEqual(has_valid_type, True)
+
+    def test_user_wrong_type(self):
+        SubscriptionFactory(user_type="random", client=self.user)
+        has_valid_type = check_if_user_has_valid_type(self.user)
+        self.assertEqual(has_valid_type, True)
+
+    def test_user_type_choice(self):
+        sub = SubscriptionFactory(client=self.user)
+        has_valid_type = check_if_user_has_valid_type(self.user)
+        print(sub.user_type)
+        self.assertEqual(has_valid_type, True)
--- subscriptions/utils.py
+++ subscriptions/utils.py
@@ -1,4 +1,8 @@
+from datetime import timedelta
+
 from django.utils import timezone
+
+from subscriptions.models import Subscription
 
 
 def check_if_user_has_valid_subscription(user):
@@ -6,3 +10,26 @@ def check_if_user_has_valid_subscription(user):
         is_active=True,
         sub_period__gte=timezone.now().date()
     ).exists()
+
+def check_if_valid_monthly_subscription_is_added(user):
+    monthly_sub = Subscription.objects.get(
+        client=user,
+        billing_type='monthly'
+    )
+    if monthly_sub:
+        monthly_sub.sub_period += timedelta(weeks=4)
+        monthly_sub.save()
+        return True
+    else:
+        return False
+
+def check_if_user_has_valid_type(user):
+    if user.subscription_set.filter(
+            user_type="individual").exists():
+        return True
+    elif user.subscription_set.filter(
+            user_type="business"
+    ).exists():
+        return True
+    else:
+        return False
