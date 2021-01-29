d2dfbca8fce24c10709fb2947c103db994ca1197
erobakiewicz <erobakiewicz@gmail.com>
2021-01-29 19:51:11 +0100

    serializer, admin and viewset for Order

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from orders.models import Order
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('subscription',
+                  'order_status',
+                  'created',
+                  'updated',
+                  'email',
+                  'price',
+        )
+    list_filter = ('subscription', 'order_status')
--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from orders.models import Order
+from orders.serializers import OrderSerializer
+
+
+class OrderViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsAuthenticated,)
+    serializer_class = OrderSerializer
+    queryset = Order.objects.all()
+
+    def get_queryset(self):
+        return super().get_queryset().filter(subscription__client=self.request.user)
